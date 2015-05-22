eaca3a42c21c1ca991fa6c6249e01b35a264b1b9
Angular PouchDB CI <angular-pouchdb-ci@tlvince.com>
2015-05-22 10:49:43 +0000

    v2.1.0

--- angular-pouchdb.js
+++ angular-pouchdb.js
@@ -1,3 +1,7 @@
+if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === exports) {
+  module.exports = 'pouchdb';
+}
+
 (function(window, angular, undefined) {
 'use strict';
 
